c51e54bbe7215b5e5a7bc4fe93eff81b75abe43e
Tim Jacomb <tim.jacomb@hmcts.net>
2022-07-06 17:01:58 +0100

    Upgrade to node 16 and webpack 5

--- webpack/govukFrontend.js
+++ webpack/govukFrontend.js
@@ -1,19 +1,21 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const packageJson = require.resolve('govuk-frontend/package.json');
-const root = path.resolve(packageJson, '..', 'govuk');
+const allJs = require.resolve('govuk-frontend');
+
+const root = path.resolve(allJs, '..');
 const sass = path.resolve(root, 'all.scss');
-const javascript = path.resolve(root, 'all.js');
+const javascript = allJs;
 const components = path.resolve(root, 'components');
 const assets = path.resolve(root, 'assets');
 const images = path.resolve(assets, 'images');
 const fonts = path.resolve(assets, 'fonts');
 
-const copyGovukTemplateAssets = new CopyWebpackPlugin([
+const copyGovukTemplateAssets = new CopyWebpackPlugin({
+  patterns: [
   { from: images, to: 'assets/images' },
   { from: fonts, to: 'assets/fonts' },
-]);
+]});
 
 module.exports = {
   paths: { template: root, components, sass, javascript, assets },
